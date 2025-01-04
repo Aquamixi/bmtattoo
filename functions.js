2d23ceda917ec512c7f095615e862930da40ba40
Aquamixi <debastianierick@gmail.com>
2025-01-04 17:53:58 -0300

    first commit

---
+++ functions.js
@@ -0,0 +1,6 @@
+
+// Adiciona a classe para animar o efeito no carregamento
+window.addEventListener('load', () => {
+    const heroButton = document.getElementById('heroButton');
+    heroButton.classList.add('loaded');
+});
